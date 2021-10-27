514a57559b8b1848968e831de84d57f4d8e7ee13
sandaru97 <sandaruadithya@gmail.com>
2021-10-27 16:25:22 +1100

    intro design done

--- App.js
+++ App.js
@@ -1,13 +1,70 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { ImageBackground,StyleSheet, Text, View, Image } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <ImageBackground resizeMode="cover"
+            style={
+                {
+                    width: "100%",
+                    height: "100%"
+                }
+            }
+            source={
+                require('./bg.jpg')
+        }>
+    <View style={{
+     
+      "height":"100%",
+      "justify-content":"center",
+      "text-align":"center",
+      "padding":"20px",
+      "border-top":"10px solid #eb9486",
+      "border-bottom":"10px solid #eb9486",
+      "background-color":"#d7d9b1",
+    }}>
+      <Image source={require('./headphones.png')} style={{
+  // "background-color":"black",
+        "width":"200px",
+        "height":"200px",
+        "float":"center",
+        "margin":"auto",
+        "margin-bottom":"0",
+        "margin-top":"0",
+        "padding-bottom":"20px",
+      }}/><br/><br/>
+      <Text
+      style={{
+        "font-size":"7vw",
+              }}
+      ><b>Wear Headphones !</b></Text>
+      <Text
+      style={{
+        "font-size":"5vw",
+
+              }}
+      ><b>We recomend wearing headphones for the best experiance.
+        You will be jumping up and down. therefore make sure the headphones or ear buds fit well.
+      </b></Text>
+      <br/>
+      <Text
+      style={{
+        "padding-top":"10px",
+        "font-size":"10vw",
+      }}
+      ><b>
+         * * *</b></Text> 
+         <br/>
+    
+    <Text style={{
+     // "position": "fixed",
+                   "font-size":"5vw",
+//"right":"0px",
+           "float":"right",
+         }}>SKIP</Text>
+         </View>
+    </ImageBackground>
   );
 }
 
